4b6174d48c43a7de5018b8f4fea0badb35b05524
clburrus <kenturkey@gmail.com>
2018-05-21 20:48:29 -0400

    file-reader.c

--- file/file-reader.c
+++ file/file-reader.c
@@ -1,5 +1,54 @@
-int FileRead(File *pfile)
+#include <stdio.h>
+#include <malloc.h>
+
+static long int LastSizeRead;
+static void *LastMalloc;
+
+// *****************************************************************************
+// *****************************************************************************
+long int GetLastSizeRead(void)
      {
-     return 1;
+     return LastSizeRead;
+     }
+
+// *****************************************************************************
+// Reads an entire file into a malloc'ed buffer.
+//
+// Returns :
+//      non-zero : Size of malloc'd buffer.
+//      zero : Failure.
+// *****************************************************************************
+int FileReader(const char *pFullPath, void **pDest)
+     {
+     long int Size;
+     FILE *pInput;
+
+     pInput = fopen(pFullPath, "rb");
+
+     if(pInput)
+          {
+          // Get the size of the file.
+          fseek(pInput, 0L, SEEK_END);
+          Size = ftell(pInput);
+
+          if(Size)
+               {
+               rewind(pInput);
+
+               LastMalloc = malloc(Size);
+
+               *pDest = LastMalloc;
+
+               if(*pDest)
+                    {
+                    LastSizeRead = fread(*pDest, 1, Size, pInput);
+                    }
+               }
+
+          fclose(pInput);
+          return Size;
+          }
+
+     return 0;
      }
 
